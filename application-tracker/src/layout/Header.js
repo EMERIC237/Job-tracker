c86ca8140b4637bdc247d8f6cc279656f945b7d9
EMERIC237 <bestvemeric@gmail.com>
2021-08-18 21:09:36 -0400

    first commit

---
+++ application-tracker/src/layout/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import header from "./header.jpg";
+
+function Header() {
+  return (
+    <header>
+      <img src={header} alt="office"></img>
+    </header>
+  );
+}
+
+export default Header;
---
+++ application-tracker/src/layout/Layout.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from './Header'
+
+function Layout() {
+    return (
+        <div>
+            <div>
+                <Header />
+            </div>
+
+            Hello, This is the beginning of a new, cool and long adventure
+        </div>
+    )
+}
+
+export default Layout
